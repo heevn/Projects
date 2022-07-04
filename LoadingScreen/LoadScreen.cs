268e31d39ba447638ff9875e90980be04a9ca0e1
heevn <102971544+heevn@users.noreply.github.com>
2022-07-04 15:40:29 +0300

    Add files via upload

---
+++ LoadingScreen/LoadScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoadingScreen
+{
+    public partial class LoadScreen : Form
+    {
+        Timer tmr = new Timer() { Interval = 3000 };
+
+        public LoadScreen()
+        {
+            InitializeComponent();
+            tmr.Tick += tmr_Tick;
+            tmr.Start();
+        }
+ 
+        void tmr_Tick(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ SaveLoad.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjectRPVS
+{
+    internal class SaveLoad
+    {
+        internal static void Save(string a, string b, string c, string d, string e, string f, bool g, bool h)
+        {
+            Properties.Settings.Default.save1 = a;
+            Properties.Settings.Default.save2 = b;
+            Properties.Settings.Default.save3 = c;
+            Properties.Settings.Default.save4 = d;
+            Properties.Settings.Default.savex = e;
+            Properties.Settings.Default.savey = f;
+            Properties.Settings.Default.ifChecked1 = g;
+            Properties.Settings.Default.ifChecked2 = h;
+            Properties.Settings.Default.Save();
+        }
+        
+        internal static void Load(string a, string b, string c, string d, string e, string f, bool g, bool h)
+        {
+            a = Properties.Settings.Default.save1;
+            b = Properties.Settings.Default.save2;
+            c = Properties.Settings.Default.save3;
+            d = Properties.Settings.Default.save4;
+            e = Properties.Settings.Default.savex;
+            f = Properties.Settings.Default.savey;
+            g = Properties.Settings.Default.ifChecked1;
+            h = Properties.Settings.Default.ifChecked2;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
